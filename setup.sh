82c1b6678e32dc818c364945fa7f615df41347df
JeterHwang <jeter0309@gmail.com>
2023-01-24 11:57:09 +0800

    update

---
+++ setup.sh
@@ -0,0 +1,11 @@
+apt -y update
+apt -y upgrade
+apt -y install wget vim
+apt -y install gawk bison
+apt -y install ruby-full default-jdk
+apt -y install golang-go
+apt -y install r-base
+apt -y install r-cran-rocr
+apt -y install libboost-all-dev
+apt -y install libblas-dev liblapack-dev
+apt -y install gfortran
