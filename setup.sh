75d8a0f3c59fb8c6b8f21d1f277aa86a58e9b9af
JeterHwang <jeter0309@gmail.com>
2023-01-24 17:38:37 +0800

    update

--- setup.sh
+++ setup.sh
@@ -9,3 +9,16 @@ apt -y install r-cran-rocr
 apt -y install libboost-all-dev
 apt -y install libblas-dev liblapack-dev
 apt -y install gfortran
+
+cd /usr/local
+wget http://ftp.gnu.org/gnu/glibc/glibc-2.29.tar.gz
+tar -zxvf glibc-2.29.tar.gz
+cd glibc-2.29
+mkdir build
+cd build/
+../configure --prefix=/usr/local --disable-sanity-checks
+make -j18
+make install
+cd /lib/x86_64-linux-gnu
+cp /usr/local/lib/libm-2.29.so /lib/x86_64-linux-gnu/
+ln -sf libm-2.29.so libm.so.6
